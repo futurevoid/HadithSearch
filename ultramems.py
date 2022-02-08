30d3a1749ac31d7a43ef37636bcfbc9581f6c078
futurevoid <tamersaeed710@gmail.com>
2022-02-08 17:36:25 +0300

    lol

--- ultramems.py
+++ ultramems.py
@@ -1,5 +1,6 @@
 import asyncio
 import datetime as dt
+from dbm import dumb
 import json
 import os
 import random
@@ -16,7 +17,7 @@ from discord.ext.commands import Bot
 from discord.utils import get
 from matplotlib import image
 from youtube_dl import YoutubeDL as ytdl
-
+from keep_Alive import keep_alive
 
 client = discord.Client()
 
@@ -37,43 +38,18 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    elif message.content.startswith('lolTmems') or message.content.startswith('/Tmems'):
-         async with aiohttp.ClientSession() as cs:
-                async with cs.get('https://www.reddit.com/r/TechMemes/.json?limit=100') as r:
-                 res = await r.json()
-                 embed = discord.Embed(title="Tech Memes", description="Here is a tech meme", color=0x00ff00)
-                 embed.set_image(url=res['data']['children'][random.randint(0,99)]['data']['url'])
-                 await message.channel.send(embed=embed)
-    if message.content.startswith('lolmems') or message.content.startswith('/mems'):
-        async with aiohttp.ClientSession() as cs:
-            async with cs.get('https://www.reddit.com/r/memes/top/.json?sort=top&t=day') as r:
-                res = await r.json()
-                embed = discord.Embed(title="Meme GIF", description="Here is a meme gif", color=0x00ff00)
-                embed.set_image(url=res['data']['children'][random.randint(0,len(res['data']['children']))]['data']['url'])
-                await message.channel.send(embed=embed)      
-    elif message.content.startswith('lol Gmems') or message.content.startswith('/gmems'):
-        async with aiohttp.ClientSession() as cs:
-            async with cs.get('https://www.reddit.com/r/gamingmemes/.json?limit=100') as r:
-                res = await r.json()
-                embed = discord.Embed(title="Gaming Memes", description="Here is a gaming meme", color=0x00ff00)
-                embed.set_image(url=res['data']['children'][random.randint(0,99)]['data']['url'])
-                await message.channel.send(embed=embed)
-    elif message.content.startswith('lolWmems') or message.content.startswith('/wmems'):
-        async with aiohttp.ClientSession() as cs:
-            async with cs.get('https://www.reddit.com/r/wholesomememes/.json?limit=100') as r:
-                res = await r.json()
-                embed = discord.Embed(title="Wholesome Memes", description="Here is a wholesome meme", color=0x00ff00)
-                embed.set_image(url=res['data']['children'][random.randint(0,99)]['data']['url'])
-                await message.channel.send(embed=embed)
-    elif message.content.startswith('lol help') or message.content.startswith('/help'):
-        embed = discord.Embed(title="Help", description="Here is a list of commands", color=0x00ff00)
+    elif message.content.startswith('lolhelp') or message.content.startswith('/help'):
+        embed = discord.Embed(title=f"Help {message.author.mention} ".format(message), description="Here is a list of commands", color=0x00ff00)
+        embed.add_field(name="lolscamalert", value="Shows scam alert meme", inline=False)
         embed.add_field(name="lolhelp", value="Displays this message", inline=False)
-        embed.add_field(name="lolmems", value="Displays a meme gif", inline=False)
-        embed.add_field(name="lolGmems", value="Displays a gaming meme", inline=False)
-        embed.add_field(name="lolWmems", value="Displays a wholesome meme", inline=False)
-        embed.add_field(name="lolTmems", value="Displays a tech meme", inline=False)
-        await message.channel.send(embed=embed)                                              
+        await message.channel.send(embed=embed)
+    elif message.content.startswith('lolscamalert') or message.content.startswith('/scamalert'):
+        embed = discord.Embed(title="Scam Alert", description="", color=0x00ff00)
+        embed.set_image(url="images.jpg")
+        await message.channel.send(embed=embed)                                                     
 
-
-
+vr = 'du'
+vrg = vr.upper()
+keep_alive()
+client.run(f"OTQwMjEzMjU5NDMxMjAyODI2.YgEH1Q.tK4nzfIjr6iQjt7nND_D_1ktd{vrg}")
 
