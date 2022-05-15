d69f4717df5d5b229bb9d9972777d1031df87565
futurevoid <72345439+futurevoid@users.noreply.github.com>
2022-05-16 01:18:03 +0300

    Add files via upload

---
+++ hadith_search.py
@@ -0,0 +1,43 @@
+import streamlit as st
+import requests
+from bs4 import BeautifulSoup
+from math import *
+
+st.set_page_config(
+         page_title="Ex-stream-ly Cool App",
+     page_icon="🧊",
+     initial_sidebar_state="expanded"
+     
+ )
+title =st.title("Hadith")
+remove_menu_footer = """
+<style>
+#MainMenu {visibility: hidden;}
+footer { visibility:hidden; }
+</style>"""
+st.markdown(remove_menu_footer, unsafe_allow_html=True)
+#input = st.text_input("")
+input = st.sidebar.text_input("اكتب جزء من حديث")
+req = requests.get(f"https://dorar-hadith-api.herokuapp.com/api/search?value={input}")
+data = req.json()
+data_len=len(data)
+for i in range(data_len):
+    number = i
+    hadith_uncleaned = data[number]["hadith"]
+    hadith = hadith_uncleaned.replace(".", "")
+    source = data[number]["source"]
+    rawi = data[number]["el_rawi"]
+    mohdith = data[number]["el_mohdith"]
+    numpage = data[number]["number_or_page"]
+    grade = data[number]["grade"]
+    align_right_i = f"<p style='text-align:right;'>{i+1}</p>"
+    st.markdown(align_right_i, unsafe_allow_html=True)
+    st.markdown(f"<p style='text-align:right;'>{hadith}</p>", unsafe_allow_html=True)
+    align_right = f"<p style='text-align:right;'>{rawi}  |  {mohdith}  |  {source}</p>"
+    st.markdown(align_right,unsafe_allow_html=True)
+    st.markdown(f"<p style='text-align:right;'>{grade}  | {numpage}  </p>",unsafe_allow_html=True)
+    st.markdown("<br>",unsafe_allow_html=True)
+
+#color = f"<p style='color:red;'>{source}</p>"
+
+#st.markdown(color,unsafe_allow_html=True)
