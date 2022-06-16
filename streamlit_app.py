8b53858858d6c0277aa9475e050bde7f0b36f28b
futurevoid <tamersaeed710@gmail.com>
2022-06-16 03:51:36 +0300

    bug fixes

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ if "load_state" not in st.session_state:
      st.session_state.load_state = False
 if 'count' not in st.session_state:
     st.session_state.count =0
-st.session_state.count += 1
+#st.session_state.count += 1
 if st.session_state.count == 0:
     st.session_state.count = 1
 
