89080acfb8e79574e4a77b7523926fa1aacf3bce
futurevoid <tamersaeed710@gmail.com>
2022-06-16 03:12:16 +0300

    فقغهىل فخ ةشنث هف شسفشقف شف 1

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ if "load_state" not in st.session_state:
      st.session_state.load_state = False
 if 'count' not in st.session_state:
     st.session_state.count = 0
-st.session_state.count += 1
+#st.session_state.count += 1
 
 def button_hadith(increment_value=1):
     st.session_state.count += increment_value
